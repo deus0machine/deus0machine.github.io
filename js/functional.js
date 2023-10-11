f6fa68f818d4b1667c1346ba7ff245d2fe14bd8c
deus0machine <125211210+deus0machine@users.noreply.github.com>
2023-10-11 21:09:16 +0300

    modified cursor

--- js/functional.js
+++ js/functional.js
@@ -18,6 +18,58 @@ function date_time()
 
         return day+"."+month+"."+year+" "+hours+":"+minutes+":"+seconds;
     }
-setInterval(function () { document.getElementById('current_date_time_block2').innerHTML = date_time(); }, 1000);
 
-document.getElementById('current_date_time_block2').innerHTML = date_time();
+const inTimer = () => {
+    document.getElementById('current_date_time_block2').innerHTML = date_time();
+}
+
+inTimer();
+setInterval(inTimer, 1000);
+    
+const lerp = (a, b, n) => (1 - n) * a + n * b;
+
+class Cursor {
+  constructor() {
+    // config
+    this.target = { x: 0.5, y: 0.5 }; // mouse position
+    this.cursor = { x: 0.5, y: 0.5 }; // cursor position
+    this.speed = 0.15;
+    this.init();
+  }
+  bindAll() {
+    ["onMouseMove", "render"].forEach((fn) => (this[fn] = this[fn].bind(this)));
+  }
+  onMouseMove(e) {
+    //get normalized mouse coordinates [0, 1]
+    this.target.x = e.clientX / window.innerWidth;
+    this.target.y = e.clientY / window.innerHeight;
+    // trigger loop if no loop is active
+    if (!this.raf) this.raf = requestAnimationFrame(this.render);
+  }
+  render() {
+    //calculate lerped values
+    this.cursor.x = lerp(this.cursor.x, this.target.x, this.speed);
+    this.cursor.y = lerp(this.cursor.y, this.target.y, this.speed);
+    document.documentElement.style.setProperty("--cursor-x", this.cursor.x);
+    document.documentElement.style.setProperty("--cursor-y", this.cursor.y);
+    //cancel loop if mouse stops moving
+    const delta = Math.sqrt(
+      Math.pow(this.target.x - this.cursor.x, 2) +
+      Math.pow(this.target.y - this.cursor.y, 2)
+    );
+    if (delta < 0.001) {
+      cancelAnimationFrame(this.raf);
+      this.raf = null;
+      return;
+    }
+    //or continue looping if mouse is moving
+    this.raf = requestAnimationFrame(this.render);
+  }
+  init() {
+    this.bindAll();
+    window.addEventListener("mousemove", this.onMouseMove);
+    this.raf = requestAnimationFrame(this.render);
+  }
+}
+
+new Cursor();
