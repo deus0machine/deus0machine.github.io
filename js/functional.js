cfe8f6d3e599eb2fa11a08a3b26fdf306f8e3c44
deus0machine <aburgomeistus@yandex.ru>
2023-11-07 16:22:09 +0300

    v0.84
    Add cursor trace

--- js/functional.js
+++ js/functional.js
@@ -1,3 +1,4 @@
+
 function zero_first_format(value)
     {
         if (value < 10)
@@ -73,5 +74,38 @@ class Cursor {
 }
 
 new Cursor();
+
+(function() {
+  "use strict";
+
+  document.onmousemove = handleMouseMove;
+  function handleMouseMove(event) {
+    var dot, eventDoc, doc, body, pageX, pageY;
+    
+    event = event || window.event; // IE-ism
+    if (event.pageX == null && event.clientX != null) {
+      eventDoc = (event.target && event.target.ownerDocument) || document;
+      doc = eventDoc.documentElement;
+      body = eventDoc.body;
+
+      event.pageX = event.clientX +
+        (doc && doc.scrollLeft || body && body.scrollLeft || 0) -
+        (doc && doc.clientLeft || body && body.clientLeft || 0);
+      event.pageY = event.clientY +
+        (doc && doc.scrollTop  || body && body.scrollTop  || 0) -
+        (doc && doc.clientTop  || body && body.clientTop  || 0 );
+    }
+
+    // Add a dot to follow the cursor
+    dot = document.createElement('div');
+    dot.className = "dot";
+    dot.style.left = event.pageX -10 + "px";
+    dot.style.top = event.pageY -10+ "px";
+    document.body.appendChild(dot);
+    setTimeout(function () {
+      dot.remove();
+   }, 250)
+  }
+})();
 
 hs.graphicsDir = 'css/graphics/';
